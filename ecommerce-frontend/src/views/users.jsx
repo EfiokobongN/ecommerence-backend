7d2c7541549283e8f67f868d30ea547876b7ee53
EfiokobongN <mailpreciousnelson@gmail.com>
2024-10-02 17:08:29 +0100

    user login and user resource

--- ecommerce-frontend/src/views/users.jsx
+++ ecommerce-frontend/src/views/users.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import axiosClient from "../axiosClient";
+
 export default function Users() {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() =>{
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+          .then(({ data }) => {
+            setLoading(false)
+            setUsers(data.data)
+          })
+          .catch(() => {
+            setLoading(false)
+          })
+      }
     return(
         <div>
             <h1>users registered</h1>
